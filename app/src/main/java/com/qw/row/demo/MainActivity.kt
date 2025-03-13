2eab3c5bd2fa1a1905bccfcb1e819c87c90c8b90
qinwei <qinwei@agora.io>
2025-03-13 17:01:52 +0800

    FEAT 示例中新增CardGroupView扩展，可以自定义group的样式

--- app/src/main/java/com/qw/row/demo/MainActivity.kt
+++ app/src/main/java/com/qw/row/demo/MainActivity.kt
@@ -10,6 +10,7 @@ import com.qw.row.ContainerView
 import com.qw.row.core.AbsGroupDescriptor
 import com.qw.row.core.AbsRowView
 import com.qw.row.core.OnRowChangedListener
+import com.qw.row.demo.ext.CardGroupDescriptor
 import com.qw.row.group.GroupDescriptor
 import com.qw.row.item.AndroidDescriptor
 import com.qw.row.item.SwitchDescriptor
@@ -44,12 +45,28 @@ class MainActivity : AppCompatActivity(), OnRowChangedListener {
         groups.add(genGroup().apply {
             addRow(AndroidDescriptor(ROW_SETTING, R.drawable.more_setting, "设置"))
         })
-        groups.add(genGroup().apply {
+
+        groups.add(genGroupCard().apply {
+            addRow(AndroidDescriptor(ROW_BANK, R.drawable.more_my_bank_card, "银行卡"))
+        })
+        groups.add(genGroupCard().apply {
+            addRow(AndroidDescriptor(ROW_COLLECT, R.drawable.more_my_favorite, "收藏"))
+            addRow(AndroidDescriptor(ROW_FRIEND, R.drawable.more_my_album, "朋友圈"))
+            addRow(AndroidDescriptor(ROW_EMOJI, R.drawable.more_emoji_store, "表情"))
             val isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
             addRow(SwitchDescriptor(ROW_NIGHT, R.drawable.more_setting, "暗黑模式", isChecked))
         })
         mContainerView.initData(ContainerDescriptor(groups, dip2px(10f)), this)
         mContainerView.notifyDataChanged()
+    }
+
+    private fun genGroupCard(): CardGroupDescriptor {
+        return CardGroupDescriptor()
+            .setDividerColor(
+                ContextCompat.getColor(this, com.qw.row.R.color.widgets_general_row_line)
+            )
+            .setDividerHeight(dip2px(0.5f))
+            .setDividerLeftMargin(dip2px(44f))
     }
 
     private fun genGroup(title: String = ""): GroupDescriptor {
--- app/src/main/java/com/qw/row/demo/ext/CardGroupView.kt
+++ app/src/main/java/com/qw/row/demo/ext/CardGroupView.kt
@@ -1,36 +1,26 @@
-package com.qw.row.group
+package com.qw.row.demo.ext
 
 import android.content.Context
-import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
-import androidx.core.view.isVisible
-import com.qw.row.R
 import com.qw.row.core.AbsGroupDescriptor
 import com.qw.row.core.AbsGroupView
 import com.qw.row.core.AbsRowDescriptor
+import com.qw.row.demo.R
 
-class GroupView : AbsGroupView<GroupDescriptor> {
+class CardGroupView(context: Context) : AbsGroupView<CardGroupDescriptor>(context) {
     private var mGroupView: LinearLayout
-    private var mGroupViewTitleLabel: TextView
-
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.widget_group_general, this)
+        LayoutInflater.from(context).inflate(R.layout.widget_group_card, this)
         mGroupView = findViewById(R.id.mGroupViewContainer)
-        mGroupViewTitleLabel = findViewById(R.id.mGroupViewTitleLabel)
     }
 
     override fun notifyDataChanged() {
         if (groupDescriptor.rowDescriptors.size > 0) {
-            mGroupViewTitleLabel.isVisible = groupDescriptor.title.isNotEmpty()
-            mGroupViewTitleLabel.text = groupDescriptor.title
             if (groupDescriptor.backgroundColor != 0) {
                 mGroupView.setBackgroundColor(groupDescriptor.backgroundColor)
             }
@@ -63,7 +53,7 @@ class GroupView : AbsGroupView<GroupDescriptor> {
     }
 }
 
-class GroupDescriptor(val title: String = "") : AbsGroupDescriptor() {
+class CardGroupDescriptor : AbsGroupDescriptor() {
     internal var background: Int = 0
     internal var backgroundColor: Int = 0
     internal val rowDescriptors = ArrayList<AbsRowDescriptor>()
@@ -72,42 +62,42 @@ class GroupDescriptor(val title: String = "") : AbsGroupDescriptor() {
     internal var dividerHeight = 0
     internal var dividerLeftMargin = 0
 
-    fun addRow(descriptor: AbsRowDescriptor): GroupDescriptor {
+    fun addRow(descriptor: AbsRowDescriptor): CardGroupDescriptor {
         rowDescriptors.add(descriptor)
         return this
     }
 
-    fun setBackground(@DrawableRes background: Int): GroupDescriptor {
+    fun setBackground(@DrawableRes background: Int): CardGroupDescriptor {
         this.background = background
         return this
     }
 
-    fun setBackgroundColor(@ColorInt backgroundColor: Int): GroupDescriptor {
+    fun setBackgroundColor(@ColorInt backgroundColor: Int): CardGroupDescriptor {
         this.backgroundColor = backgroundColor
         return this
     }
 
-    fun setDividerColor(color: Int): GroupDescriptor {
+    fun setDividerColor(color: Int): CardGroupDescriptor {
         this.dividerColor = color
         return this
     }
 
-    fun setDividerHeight(height: Int): GroupDescriptor {
+    fun setDividerHeight(height: Int): CardGroupDescriptor {
         this.dividerHeight = height
         return this
     }
 
-    fun setDividerLeftMargin(dividerLeftMargin: Int): GroupDescriptor {
+    fun setDividerLeftMargin(dividerLeftMargin: Int): CardGroupDescriptor {
         this.dividerLeftMargin = dividerLeftMargin
         return this
     }
 
-    fun setDividerResource(resourceId: Int): GroupDescriptor {
+    fun setDividerResource(resourceId: Int): CardGroupDescriptor {
         this.dividerResource = resourceId
         return this
     }
 
     override fun getViewClass(): Class<*> {
-        return GroupView::class.java
+        return CardGroupView::class.java
     }
 }
